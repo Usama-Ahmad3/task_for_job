e194c81e5aef0226af48121b7a647c63368a628c
Usama Ahmad <usama.ahmad435750@gmail.com>
2024-01-15 22:50:14 +0500

    git actions

--- lib/res/app_colors/app_colors.dart
+++ lib/res/app_colors/app_colors.dart
@@ -8,5 +8,4 @@ class AppColors {
   static Color blue = Colors.blue;
   static Color grey300 = Colors.grey.shade300;
   static Color grey = Colors.grey;
-  static Color grey200 = Colors.grey.shade200;
 }
